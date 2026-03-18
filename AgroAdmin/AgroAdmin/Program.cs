5438a3476f42f13a9404ba4c5cf47bc92ddedee6
Alexander Rudich <rudichalex@yandex.ru>
2026-03-18 14:57:03 +0300

    Revert "ci: улучшение деплоя очистка NuGet кэша, проверка версии SqlClient 6.…" (#31)
    This reverts commit 38855f222a57fc46297a32309491ef0d84aaa09a.

--- AgroAdmin/AgroAdmin/Program.cs
+++ AgroAdmin/AgroAdmin/Program.cs
@@ -201,12 +201,6 @@ app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(AgroAdmin.Client._Imports).Assembly);
 
-app.MapGet("/api/debug/version", () =>
-{
-    var assembly = typeof(Microsoft.Data.SqlClient.SqlConnection).Assembly;
-    var version = assembly.GetName().Version?.ToString() ?? "unknown";
-    var location = assembly.Location;
-    return new { version, location };
-});
+app.MapGet("/health", () => Results.Ok("Healthy"));
 
 app.Run();
