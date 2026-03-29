503d434fe877f066360a6cde93c08bc641a8c238
Alexander Rudich <rudichalex@yandex.ru>
2026-03-29 12:26:12 +0300

    Fixes/small issues (#54)
    * исправлено текстовое описание частичной загрузки
    
    * теперь на телефоне текст вмещается в кнопки для bookingsummary

--- AgroAdmin.Shared/Dto/Telegram/Responses/DailyAvailability.cs
+++ AgroAdmin.Shared/Dto/Telegram/Responses/DailyAvailability.cs
@@ -7,9 +7,13 @@
         public bool IsParkingSideFree { get; set; }
         public bool IsWholeHouseFree { get; set; }
 
-        // Вспомогательные свойства (можно вычислить в коде, не обязательно в JSON)
+        // Полностью свободно - все три объекта свободны
         public bool IsFullyFree => IsPondSideFree && IsParkingSideFree && IsWholeHouseFree;
-        public bool IsPartiallyFree => (IsPondSideFree || IsParkingSideFree || IsWholeHouseFree) && !IsFullyFree;
+
+        // Полностью занято - нет свободных объектов (все три заняты)
         public bool IsFullyOccupied => !IsPondSideFree && !IsParkingSideFree && !IsWholeHouseFree;
+
+        // Частично занято - свободен ровно один объект, остальные два заняты
+        public bool IsPartiallyFree => !IsFullyFree && !IsFullyOccupied;
     }
 }
